d9668810790c3ca1c52f742e3919fe250867b4b4
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2018-09-03 18:07:09 +0530

    Add files via upload

---
+++ PosNeg.java
@@ -0,0 +1,17 @@
+package Guvi;
+import java.util.*;
+public class PosNeg {
+	public static void main(String args[]) {
+     int n;
+     System.out.println("enter the number");
+     Scanner sc = new Scanner(System.in);
+     n=sc.nextInt();
+     if( n < 0) {
+    	 System.out.println("negative number");
+     }
+     else {
+    	 System.out.println("positive number");
+     }
+
+}
+}
