486b299ba021e285f71f53732d65d94d5cddc453
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2018-09-04 17:09:33 +0530

    Create EvenorOdd.java

---
+++ EvenorOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class EvenorOdd {
+public static void main(String args[]) {
+int n;
+System.out.println("enter the number");
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n%2==0){
+	System.out.println("even");
+}
+else{
+	System.out.println("invalid");
+}
+
+}
+}
