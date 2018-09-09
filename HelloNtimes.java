7f2520a3cee17fd0b2924a5d9fec4b8dd9bed461
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2018-09-09 12:05:35 +0530

    Create HelloNtimes.java

---
+++ HelloNtimes.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HelloNtimes {
+public static void main(String args[]) {
+	int N;
+	System.out.println("input :");
+	Scanner sc=new Scanner(System.in);
+	N=sc.nextInt();
+	for(int i=1;i <= N;i++) {
+		System.out.println("Hello");
+	}
+}
+}
