dbea88f995849c0d22c08e1fb2fe6d1b21ae95c9
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-01-28 19:08:46 +0530

    Create Oddintervals.java

---
+++ Oddintervals.java
@@ -0,0 +1,17 @@
+package mercy;
+import java.util.*;
+public class Oddintervals {
+public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int a=sc.nextInt();
+	for(int i=n+1;i<a;i++) {
+		if(i%2!=0) {
+			System.out.println( i+ " ");
+		}
+	
+	}
+	
+			
+}
+}
