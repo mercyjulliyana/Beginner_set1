039849b1bceb9ec8b0003bc6239a76e526030c04
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-05-04 11:43:49 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,24 @@
+package merc;
+import java.util.Scanner;
+public class Armstrong {
+public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+    int temp=n;
+    int sum=0;
+    while(n>0) {
+    	int rem=n%10;
+    	sum=sum+(rem*rem*rem);
+    	n=n/10;
+    }
+    if(temp==sum) {
+    	System.out.println("Armstrong number");
+    }else {
+    	System.out.println("Not armstrong number");
+    
+    }
+	
+	
+	
+}
+}
