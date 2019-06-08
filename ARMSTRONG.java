95b72c4103e61a6e1fd921bef1bb446e7f6f3bfa
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-06-08 11:45:49 +0530

    Create ARMSTRONG.java

---
+++ ARMSTRONG.java
@@ -0,0 +1,25 @@
+package Guvi;
+import java.util.*;
+public class ARMSTRONG {
+public static void main(String args[]){
+	Scanner sc=new Scanner (System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	
+	int sum=0;
+	for(int i=a+1;i<=b;i++){
+	int	temp=i;
+	sum=0;
+		while(temp!=0){
+			int rem=temp%10;
+		    sum=sum+(rem*rem*rem);
+		    temp=temp/10;
+			
+		}
+	
+		if(sum==i){
+		System.out.println(i);
+	}
+}
+}
+}
