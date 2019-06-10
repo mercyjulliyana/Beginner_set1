ce8d22bba3d903c0bd6c22a5f3632c15fa503733
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-06-10 09:43:38 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,14 @@
+package Guvi;
+import java.util.*;
+public class Count {
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	String str=sc.nextLine();
+	int count=0;
+	for(int i=0;i<=str.length();i++){
+		count++;
+		
+	}
+	System.out.println(count);
+}
+}
