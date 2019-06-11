7b5523156350eeb8f0f79ec75f835fde9723259d
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-06-11 12:18:07 +0530

    Create SWAP2NUm.java

---
+++ SWAP2NUm.java
@@ -0,0 +1,16 @@
+package Guvi;
+import java.util.*;
+public class SWAP2NUm {
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	int temp=0;
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println(a);
+	System.out.println(b);
+	}
+}
