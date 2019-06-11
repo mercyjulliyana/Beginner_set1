395b59890941ab730fd61dcf42b0eda142bfa6a8
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-06-11 15:42:46 +0530

    Create HOLIDAY.java

---
+++ HOLIDAY.java
@@ -0,0 +1,25 @@
+package Guvi;
+import java.util.*;
+public class HOLIDAY {
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	String str=sc.nextLine();
+	switch(str){
+	
+	case "Monday":
+	case "Tuesday":
+	case "Wednesday":
+		case "Thursday":
+	case "Friday":System.out.println("No");
+	break;
+	case "Sunday":
+	case "Saturday":System.out.println("Yes");
+	break;
+	
+    
+
+
+
+	}
+}
+}
