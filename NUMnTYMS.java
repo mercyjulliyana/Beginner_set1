cb1c152f2e3f30a61d25353aac0c715437e29b46
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-06-12 14:50:04 +0530

    Create NUMnTYMS.java

---
+++ NUMnTYMS.java
@@ -0,0 +1,12 @@
+package Guvi;
+import java.util.*;
+public class NUMnTYMS {
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	String str=sc.nextLine();
+	int n=sc.nextInt();
+	for(int i=1;i<=n;i++){
+		System.out.println(str);
+	}
+}
+}
