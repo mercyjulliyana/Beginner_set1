483edaf06594d56b33ae7ad1d944e77e67cd630e
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-07-04 09:14:43 +0530

    Create CheckGreatStr.java

---
+++ CheckGreatStr.java
@@ -0,0 +1,19 @@
+package Guvi;
+import java.util.*;
+public class CheckGreatStr {
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	String s=sc.nextLine();
+	String a=sc.nextLine();
+	String str=" ";
+	if(s.length()>a.length()){
+		System.out.println(s);
+		}else{
+			System.out.println(a);
+	
+	}
+		
+		
+	}
+}
+
