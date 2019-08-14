3b977be124cfa9708ec05ac9ab4a01cfc109d66e
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-08-14 17:55:42 +0530

    Create ChARRRR.java

---
+++ ChARRRR.java
@@ -0,0 +1,10 @@
+package BAs;
+import java.util.*;
+public class ChARRRR {
+public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    char c=(char)n;
+    System.out.println(c);
+}
+}
