db341c0fb7af3dfe2982e4e08072076abe23d66e
mercyjulliyana <42141686+mercyjulliyana@users.noreply.github.com>
2019-08-15 10:27:45 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Number{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>1&&n<=10){
+            System.out.println("Yes");
+            
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
